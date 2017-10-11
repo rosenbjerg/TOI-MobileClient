1816839b7e01c0da616376277c0a8bae8d605121
Unknown <tmorel14@student.aau.dk>
2017-10-11 10:58:58 +0200

    Added test project

---
+++ TOI-MobileClient/TOI_MobileClient/RestClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace TOI_MobileClient
+{
+    
+    class RestClient
+    {
+        HttpClient _client;
+    }
+}
---
+++ TOI_MobileClient.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TOI_MobileClient.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TOI_MobileClient.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8038424d-606f-4678-aaa6-ce2ab211ec4a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
