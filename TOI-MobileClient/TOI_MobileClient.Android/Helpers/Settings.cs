06eb6229de96aa7e28a01ae2e7825b4be77eeead
Marius Lauge Nørgaard <mariuslauge@gmail.com>
2017-10-20 11:00:40 +0200

    Slight changes in settings.cs before merge.

--- TOI-MobileClient/TOI_MobileClient.Android/Helpers/Settings.cs
+++ TOI-MobileClient/TOI_MobileClient.Android/Helpers/Settings.cs
@@ -1,16 +1,14 @@
-/*
-// Helpers/Settings.cs This file was automatically added when you installed the Settings Plugin. If you are not using a PCL then comment this file back in to use it.
 using Plugin.Settings;
 using Plugin.Settings.Abstractions;
 
 namespace TOI_MobileClient.Droid.Helpers
 {
-	/// <summary>
-	/// This is the Settings static class that can be used in your Core solution or in any
-	/// of your client applications. All settings are laid out the same exact way with getters
-	/// and setters. 
-	/// </summary>
-	public static class Settings
+    /// <summary>
+    /// This is the Settings static class that can be used in your Core solution or in any
+    /// of your client applications. All settings are laid out the same exact way with getters
+    /// and setters. 
+    /// </summary>
+    public static class Settings
 	{
 		private static ISettings AppSettings
 		{
@@ -41,4 +39,4 @@ namespace TOI_MobileClient.Droid.Helpers
 		}
 
 	}
-}*/
+}
