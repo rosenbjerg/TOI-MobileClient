a74ea8efbe912d55174e849f523efe0e15ee7afd
Malte Rosenbjerg <malterandersen@live.dk>
2017-10-26 11:05:31 +0200

    added TapViewCell

---
+++ TOI-MobileClient/TOI_MobileClient/TapViewCell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TOI_MobileClient
+{
+    class TapViewCell : ViewCell
+    {
+        public ICommand Command { get; set; }
+
+        public TapViewCell()
+        {
+            Tapped += (sender, args) =>
+            {
+                if (Command?.CanExecute(null) ?? false)
+                    Command.Execute(null);
+            };
+        }
+    }
+}
