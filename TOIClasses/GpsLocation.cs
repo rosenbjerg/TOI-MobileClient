5b219a17190a4e43a14fbbe2739475236dc16406
Tobias Morell <tmorel14@student.aau.dk>
2017-11-30 14:52:09 +0100

    Some minor changes to TOIClasses

---
+++ TOIClasses/GpsLocation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TOIClasses
+{
+    public class GpsLocation
+    {
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
---
+++ TOIClasses/StaticFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TOIClasses
+{
+    public class StaticFile : ModelBase
+    {
+        public string Filetype { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public StaticFile()
+        {
+            CreatedOn = DateTime.Now;
+        }
+
+        public string GetFilename()
+        {
+            return Id + "." + Filetype;
+        }
+    }
+}
