9d4bd03d06dbec99badb33e4668cc666a88d4df1
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2017-12-06 11:19:20 +0100

    subscription manager started

---
+++ TOI-MobileClient/TOI_MobileClient/Managers/SubscriptionManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TOIClasses;
+
+namespace TOI_MobileClient.Managers
+{
+    class SubscriptionManager
+    {
+        List<SubscribedServer> _subscribedServers = new List<SubscribedServer>();
+
+        public IEnumerable<string> AllTags => _subscribedServers.SelectMany(ss => ss.TagCache);
+        
+    }
+
+    class SubscribedServer
+    {
+        /// <summary>
+        /// When inside radius of feed server
+        /// </summary>
+        public bool Active { get; set; } = true;
+        /// <summary>
+        /// When the servers tois should be hidden (inactive) e
+        /// </summary>
+        public bool Hidden { get; set; } 
+        public string Name { get; set; }
+        public string BaseUrl { get; set; }
+
+        public List<string> Contexts = new List<string>();
+        
+        public async Task LoadTois()
+        {
+            var url = $"{BaseUrl}/tois?contexts={string.Join(",", Contexts)}";
+            ToiCache = await ToiHttpClient.Instance.GetMany<ToiModel>(url);
+            TagCache = ToiCache.SelectMany(toi => toi.Tags).ToHashSet();
+            Index = TagCache.ToDictionary(tagId => tagId,
+                tagId => ToiCache.Where(toi => toi.Tags.Contains(tagId)).ToList());
+        }
+
+        public Dictionary<string, List<ToiModel>> Index { get; set; }
+
+        public List<ToiModel> ToiCache { get; private set; } 
+        public HashSet<string> TagCache = new HashSet<string>();
+        
+    }
+}
---
+++ TOI-MobileClient/TOI_MobileClient/Managers/ToiHttpClient.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TOIClasses;
+
+namespace TOI_MobileClient.Managers
+{
+    class ToiHttpClient
+    {
+        private static ToiHttpClient _instance;
+        public static ToiHttpClient Instance => _instance ?? (_instance = new ToiHttpClient());
+
+        private ToiHttpClient()
+        {
+            
+        }
+
+        private readonly HttpClient _http = new HttpClient();
+
+        public async Task<TModel> Get<TModel>(string url, Dictionary<string, string> queries = null)
+            where TModel : ModelBase
+        {
+            var res = await _http.GetAsync(url + GetAsQuery(queries));
+            if (!res.IsSuccessStatusCode)
+                return null;
+            try
+            {
+                var list = JsonConvert.DeserializeObject<TModel>(await res.Content.ReadAsStringAsync());
+                return list;
+            }
+            catch (JsonException e)
+            {
+                return null;
+            }
+        }
+        public async Task<List<TModel>> GetMany<TModel>(string url, Dictionary<string, string> queries = null)
+            where TModel : ModelBase
+        {
+            var res = await _http.GetAsync(url + GetAsQuery(queries));
+            if (!res.IsSuccessStatusCode)
+                return null;
+            try
+            {
+                var list = JsonConvert.DeserializeObject<List<TModel>>(await res.Content.ReadAsStringAsync());
+                return list;
+            }
+            catch (JsonException e)
+            {
+                return null;
+            }
+        }
+
+        private static string GetAsQuery(Dictionary<string, string> queries)
+        {
+            if (queries != null)
+                return "?" + string.Join("&", queries.Keys.Select(key => $"{key}={queries[key]}"));
+            return "";
+        }
+    }
+}
